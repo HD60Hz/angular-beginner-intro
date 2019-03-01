f8defdba4389cbf4dd7d9352897723f21a2c2518
INELHAJ IBRAHIM <ibrahim.inelhaj@open-groupe.com>
2019-03-01 17:15:45 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,10 @@
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//
-//   newTodo: string;
-//   todos: any;
-//   todoObj: any;
-//
-//   constructor() {
-//     this.newTodo = '';
-//     this.todos = [];
-//   }
-//
-//   addTodo(event) {
-//     this.todoObj = {
-//       newTodo: this.newTodo,
-//       completed: false
-//     }
-//     this.todos.push(this.todoObj);
-//     this.newTodo = '';
-//     event.preventDefault();
-//   }
-//
-//   deleteTodo(index) {
-//     this.todos.splice(index, 1);
-//   }
-//
-//   deleteSelectedTodos() {
-//     for(let i = (this.todos.length -1 ); i > -1; i--) {
-//       if(this.todos[i].completed) {
-//         this.todos.splice(i, 1);
-//       }
-//     }
-//   }
-// }
-
-
-
-// --
-
 import { Component, OnInit } from '@angular/core';
-import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('moveInLeft', [
-      transition('void=> *', [style({transform: 'translateX(300px)'}),
-        animate('200ms ease-out', keyframes([
-          style({transform: 'translateX(300px)'}),
-          style({transform: 'translateX(0)'})
-
-        ]))]),
-      transition('*=>void', [style({transform: 'translateX(0px)'}),
-        animate('250ms ease-in',   keyframes([
-          style({transform: 'translateY(-20px)', opacity: 1, offset: 0.2}),
-          style({transform: 'translateY(250px)', opacity: 0 , offset: 1})
-
-        ]))])
-
-    ])
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
   todoArray: string[] = [];
